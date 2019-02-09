b276e593cc1cebc3bb87fb73550ba01e98d4d82a
Eric Scouten <eric@scouten.com>
2019-02-08 20:49:46 -0800

    Remove BinaryBlobError since we're not implementing line_map.

--- lib/xgit/errors/binary_blob_error.ex
+++
@@ -1,9 +0,0 @@
-defmodule Xgit.Errors.BinaryBlobError do
-  @moduledoc ~S"""
-  Raised when binary data was found in a context that requires text
-  (e.g. for generating textual diffs).
-  """
-  defexception [:message]
-
-  def exception(_), do: %__MODULE__{message: "A NULL byte was found where text was expected."}
-end
