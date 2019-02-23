8bc6a7ad8101093f702d93aa4473cfe21e272bbe
Eric Scouten <eric@scouten.com>
2019-02-22 20:45:44 -0800

    Remove one last dangling reference to Swarm.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,1 @@
-# The first call to register_name/2 is expensive and can cause timeout failures
-# in individual tests. Doing it first primes the pump.
-Swarm.register_name(make_ref(), self())
-
 ExUnit.start()
