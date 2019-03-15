e7afd33dfe429b5937db424052019828a2413277
Eric Scouten <eric@scouten.com>
2019-03-14 18:21:00 -0700

    Remove test case that was ported already.

--- test/xgit/storage/file/file_based_config_test.exs
+++ test/xgit/storage/file/file_based_config_test.exs
@@ -70,27 +70,6 @@ defmodule Xgit.Storage.File.FileBasedConfigTest do
     assert File.read!(path) == to_string(' \n\t' ++ @content2)
   end
 
-  #
-  # @Test
-  # public void testLeadingWhitespaces() throws IOException, ConfigInvalidException {
-  #   final ByteArrayOutputStream bos1 = new ByteArrayOutputStream();
-  #   bos1.write(" \n\t".getBytes(UTF_8));
-  #   bos1.write(CONTENT1.getBytes(UTF_8));
-  #
-  #   final File file = createFile(bos1.toByteArray());
-  #   final FileBasedConfig config = new FileBasedConfig(file, FS.DETECTED);
-  #   config.load();
-  #   assertEquals(ALICE, config.getString(USER, null, NAME));
-  #
-  #   config.setString(USER, null, NAME, BOB);
-  #   config.save();
-  #
-  #   final ByteArrayOutputStream bos2 = new ByteArrayOutputStream();
-  #   bos2.write(" \n\t".getBytes(UTF_8));
-  #   bos2.write(CONTENT2.getBytes(UTF_8));
-  #   assertArrayEquals(bos2.toByteArray(), IO.readFully(file));
-  # }
-  #
   # @Test
   # public void testIncludeAbsolute()
   #     throws IOException, ConfigInvalidException {
