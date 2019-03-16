1177cee4b7273b00ea6be656001cac7a9ed7eca8
Eric Scouten <eric@scouten.com>
2019-03-15 20:30:21 -0700

    Implement Xgit.Util.MockConfigStorage.

--- test/support/mock_system_reader.ex
+++ test/support/mock_system_reader.ex
@@ -2,6 +2,9 @@ defmodule Xgit.Util.MockSystemReader do
   @moduledoc false
   # Used for testing only.
 
+  alias Xgit.Lib.Config
+  alias Xgit.Util.MockConfigStorage
+
   defstruct hostname: "fake.host.example.com",
             env: %{},
             user_config: nil,
@@ -9,6 +12,13 @@ defmodule Xgit.Util.MockSystemReader do
             time: 1_250_379_778_668
 
   # time == Sat Aug 15 20:12:58 GMT-03:30 2009
+
+  def new do
+    %__MODULE__{
+      user_config: Config.new(storage: %MockConfigStorage{}),
+      system_config: Config.new(storage: %MockConfigStorage{})
+    }
+  end
 
   # Adjust the current time by _n_ seconds.
   def tick(%{time: time} = mock, seconds), do: %{mock | time: time + seconds * 1000}
@@ -20,14 +30,16 @@ defimpl Xgit.Util.SystemReader, for: Xgit.Util.MockSystemReader do
   def hostname(%{hostname: hostname}), do: hostname
   def get_env(%{env: env}, variable), do: Map.get(env, variable)
 
-  def user_config(%MockSystemReader{user_config: user_config} = _reader, nil = _parent_config),
-    do: user_config
+  def user_config(%MockSystemReader{user_config: user_config} = _reader, nil = _parent_config) do
+    user_config
+  end
 
   def system_config(
         %MockSystemReader{system_config: system_config} = _reader,
         nil = _parent_config
-      ),
-      do: system_config
+      ) do
+    system_config
+  end
 
   def current_time(%{time: time}), do: time
 end
