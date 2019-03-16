6ea74929a21fc1eb1ab9df0347d8cbd841b04ca4
Eric Scouten <eric@scouten.com>
2019-03-16 06:50:38 -0700

    Implement Xgit.Util.MockConfigStorage. (#57)

---
+++ test/support/mock_config_storage.ex
@@ -0,0 +1,19 @@
+defmodule Xgit.Util.MockConfigStorage do
+  @moduledoc false
+  # Used for testing only.
+
+  defstruct ignore: :me
+  # We need at least one struct member in order to define a struct,
+  # but in this case, it's there only to trigger this implementation
+  # of the protocol. The actual content doesn't matter.
+end
+
+defimpl Xgit.Lib.Config.Storage, for: Xgit.Util.MockConfigStorage do
+  # This "storage" implementation is used so that MockSystemReader can construct
+  # Config structs that will accept the `load/1` call, but we don't actually want
+  # to pollute the test state with whatever happens to be in the user or system
+  # .gitconfig files. So we replace the file-based implementations with no-ops.
+
+  def load(_storage, _config), do: :ok
+  def save(_storage, _config), do: :ok
+end
