fc1d94e7b224a2e449c1c8dbd0da7e023b71f986
Eric Scouten <eric@scouten.com>
2019-03-18 20:52:16 -0700

    Test coverage for read_environment/2.

---
+++ test/xgit/lib/base_repository_builder_test.exs
@@ -0,0 +1,87 @@
+defmodule Xgit.Lib.BaseRepositoryBuilderTest do
+  use ExUnit.Case, async: true
+
+  alias Xgit.Lib.BaseRepositoryBuilder
+  alias Xgit.Test.MockSystemReader
+
+  describe "read_environment/2" do
+    test "basic case" do
+      msr = %MockSystemReader{}
+      b = %BaseRepositoryBuilder{} |> BaseRepositoryBuilder.read_environment(msr)
+
+      assert b == %BaseRepositoryBuilder{
+               git_dir: nil,
+               object_dir: nil,
+               alternate_object_directories: nil,
+               bare?: false,
+               must_exist?: false,
+               work_tree: nil,
+               index_file: nil,
+               ceiling_directories: nil
+             }
+    end
+
+    test "read from environment" do
+      msr = %MockSystemReader{
+        env: %{
+          "GIT_DIR" => ".xgit",
+          "GIT_OBJECT_DIRECTORY" => ".objects",
+          "GIT_ALTERNATE_OBJECT_DIRECTORIES" => "alternate:object:dirs",
+          "GIT_WORK_TREE" => "work_tree",
+          "GIT_INDEX_FILE" => "gitindex",
+          "GIT_CEILING_DIRECTORIES" => "git:ceiling:dirs"
+        }
+      }
+
+      b = %BaseRepositoryBuilder{} |> BaseRepositoryBuilder.read_environment(msr)
+
+      assert b == %BaseRepositoryBuilder{
+               git_dir: ".xgit",
+               object_dir: ".objects",
+               alternate_object_directories: ["alternate", "object", "dirs"],
+               bare?: false,
+               must_exist?: false,
+               work_tree: "work_tree",
+               index_file: "gitindex",
+               ceiling_directories: ["git", "ceiling", "dirs"]
+             }
+    end
+
+    test "builder was already populated" do
+      msr = %MockSystemReader{
+        env: %{
+          "GIT_DIR" => ".xgit",
+          "GIT_OBJECT_DIRECTORY" => ".objects",
+          "GIT_ALTERNATE_OBJECT_DIRECTORIES" => "alternate:object:dirs",
+          "GIT_WORK_TREE" => "work_tree",
+          "GIT_INDEX_FILE" => "gitindex",
+          "GIT_CEILING_DIRECTORIES" => "git:ceiling:dirs"
+        }
+      }
+
+      b =
+        %BaseRepositoryBuilder{
+          git_dir: "xgit",
+          object_dir: "objects",
+          alternate_object_directories: ["object", "alternates"],
+          bare?: false,
+          must_exist?: false,
+          work_tree: "tree_of_work",
+          index_file: "index_of_git",
+          ceiling_directories: ["ceilings", "dirs"]
+        }
+        |> BaseRepositoryBuilder.read_environment(msr)
+
+      assert b == %BaseRepositoryBuilder{
+               git_dir: "xgit",
+               object_dir: "objects",
+               alternate_object_directories: ["object", "alternates"],
+               bare?: false,
+               must_exist?: false,
+               work_tree: "tree_of_work",
+               index_file: "index_of_git",
+               ceiling_directories: ["ceilings", "dirs"]
+             }
+    end
+  end
+end
