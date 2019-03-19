e0fa091743a73c8431da4ab2491b2b7ccbc0bb04
Eric Scouten <eric@scouten.com>
2019-03-18 17:39:36 -0700

    Simplify away all the getters and setters to struct manipulation.

--- lib/xgit/lib/base_repository_builder.ex
+++ lib/xgit/lib/base_repository_builder.ex
@@ -3,426 +3,105 @@ defmodule Xgit.Lib.BaseRepositoryBuilder do
   A behaviour module for implementing specialized repository construction.
 
   An implementation of this behaviour may add custom repository detection methods.
+
+  Struct members:
+  * `git_dir`: The `.git` directory storing the repository metadata.
+  * `object_dir`: The directory storing the repository's objects.
+  * `alternate_object_directories`: List of alternate object directories to search.
+  * `bare?`: True only if the caller wants to force bare behavior.
+  * `must_exist?`: True if the caller requires the repository to exist.
+  * `work_tree`: The top level directory of the working files.
+  * `index_file`: The local index file that is caching checked out file status.
+  * `ceiling_directories`: A list of directories limiting the search for a Git repository.
+
+  PORTING NOTE: Unlike the jgit implementation, this version has no setters or getters
+  for the configuration options. Set up the struct directly and then call the
+  functions on this module to set up the repository.
   """
 
-  # private static boolean isSymRef(byte[] ref) {
-  #   if (ref.length < 9)
-  #     return false;
-  #   return /**/ref[0] == 'g' //
-  #       && ref[1] == 'i' //
-  #       && ref[2] == 't' //
-  #       && ref[3] == 'd' //
-  #       && ref[4] == 'i' //
-  #       && ref[5] == 'r' //
-  #       && ref[6] == ':' //
-  #       && ref[7] == ' ';
-  # }
-  #
-  # private static File getSymRef(File workTree, File dotGit, FS fs)
-  #     throws IOException {
-  #   byte[] content = IO.readFully(dotGit);
-  #   if (!isSymRef(content))
-  #     throw new IOException(MessageFormat.format(
-  #         JGitText.get().invalidGitdirRef, dotGit.getAbsolutePath()));
-  #
-  #   int pathStart = 8;
-  #   int lineEnd = RawParseUtils.nextLF(content, pathStart);
-  #   while (content[lineEnd - 1] == '\n' ||
-  #          (content[lineEnd - 1] == '\r' && SystemReader.getInstance().isWindows()))
-  #     lineEnd--;
-  #   if (lineEnd == pathStart)
-  #     throw new IOException(MessageFormat.format(
-  #         JGitText.get().invalidGitdirRef, dotGit.getAbsolutePath()));
-  #
-  #   String gitdirPath = RawParseUtils.decode(content, pathStart, lineEnd);
-  #   File gitdirFile = fs.resolve(workTree, gitdirPath);
-  #   if (gitdirFile.isAbsolute())
-  #     return gitdirFile;
-  #   else
-  #     return new File(workTree, gitdirPath).getCanonicalFile();
-  # }
-  #
-  # private FS fs;
-  #
-  # private File gitDir;
-  #
-  # private File objectDirectory;
-  #
-  # private List<File> alternateObjectDirectories;
-  #
-  # private File indexFile;
-  #
-  # private File workTree;
-  #
-  # /** Directories limiting the search for a Git repository. */
-  # private List<File> ceilingDirectories;
-  #
-  # /** True only if the caller wants to force bare behavior. */
-  # private boolean bare;
-  #
-  # /** True if the caller requires the repository to exist. */
-  # private boolean mustExist;
-  #
-  # /** Configuration file of target repository, lazily loaded if required. */
-  # private Config config;
-  #
-  # /**
-  #  * Set the file system abstraction needed by this repository.
-  #  *
-  #  * @param fs
-  #  *            the abstraction.
-  #  * @return {@code this} (for chaining calls).
-  #  */
-  # public B setFS(FS fs) {
-  #   this.fs = fs;
-  #   return self();
-  # }
-  #
-  # /**
-  #  * Get the file system abstraction, or null if not set.
-  #  *
-  #  * @return the file system abstraction, or null if not set.
-  #  */
-  # public FS getFS() {
-  #   return fs;
-  # }
-  #
-  # /**
-  #  * Set the Git directory storing the repository metadata.
-  #  * <p>
-  #  * The meta directory stores the objects, references, and meta files like
-  #  * {@code MERGE_HEAD}, or the index file. If {@code null} the path is
-  #  * assumed to be {@code workTree/.git}.
-  #  *
-  #  * @param gitDir
-  #  *            {@code GIT_DIR}, the repository meta directory.
-  #  * @return {@code this} (for chaining calls).
-  #  */
-  # public B setGitDir(File gitDir) {
-  #   this.gitDir = gitDir;
-  #   this.config = null;
-  #   return self();
-  # }
-  #
-  # /**
-  #  * Get the meta data directory; null if not set.
-  #  *
-  #  * @return the meta data directory; null if not set.
-  #  */
-  # public File getGitDir() {
-  #   return gitDir;
-  # }
-  #
-  # /**
-  #  * Set the directory storing the repository's objects.
-  #  *
-  #  * @param objectDirectory
-  #  *            {@code GIT_OBJECT_DIRECTORY}, the directory where the
-  #  *            repository's object files are stored.
-  #  * @return {@code this} (for chaining calls).
-  #  */
-  # public B setObjectDirectory(File objectDirectory) {
-  #   this.objectDirectory = objectDirectory;
-  #   return self();
-  # }
-  #
-  # /**
-  #  * Get the object directory; null if not set.
-  #  *
-  #  * @return the object directory; null if not set.
-  #  */
-  # public File getObjectDirectory() {
-  #   return objectDirectory;
-  # }
-  #
-  # /**
-  #  * Add an alternate object directory to the search list.
-  #  * <p>
-  #  * This setting handles one alternate directory at a time, and is provided
-  #  * to support {@code GIT_ALTERNATE_OBJECT_DIRECTORIES}.
-  #  *
-  #  * @param other
-  #  *            another objects directory to search after the standard one.
-  #  * @return {@code this} (for chaining calls).
-  #  */
-  # public B addAlternateObjectDirectory(File other) {
-  #   if (other != null) {
-  #     if (alternateObjectDirectories == null)
-  #       alternateObjectDirectories = new LinkedList<>();
-  #     alternateObjectDirectories.add(other);
-  #   }
-  #   return self();
-  # }
-  #
-  # /**
-  #  * Add alternate object directories to the search list.
-  #  * <p>
-  #  * This setting handles several alternate directories at once, and is
-  #  * provided to support {@code GIT_ALTERNATE_OBJECT_DIRECTORIES}.
-  #  *
-  #  * @param inList
-  #  *            other object directories to search after the standard one. The
-  #  *            collection's contents is copied to an internal list.
-  #  * @return {@code this} (for chaining calls).
-  #  */
-  # public B addAlternateObjectDirectories(Collection<File> inList) {
-  #   if (inList != null) {
-  #     for (File path : inList)
-  #       addAlternateObjectDirectory(path);
-  #   }
-  #   return self();
-  # }
-  #
-  # /**
-  #  * Add alternate object directories to the search list.
-  #  * <p>
-  #  * This setting handles several alternate directories at once, and is
-  #  * provided to support {@code GIT_ALTERNATE_OBJECT_DIRECTORIES}.
-  #  *
-  #  * @param inList
-  #  *            other object directories to search after the standard one. The
-  #  *            array's contents is copied to an internal list.
-  #  * @return {@code this} (for chaining calls).
-  #  */
-  # public B addAlternateObjectDirectories(File[] inList) {
-  #   if (inList != null) {
-  #     for (File path : inList)
-  #       addAlternateObjectDirectory(path);
-  #   }
-  #   return self();
-  # }
-  #
-  # /**
-  #  * Get ordered array of alternate directories; null if non were set.
-  #  *
-  #  * @return ordered array of alternate directories; null if non were set.
-  #  */
-  # public File[] getAlternateObjectDirectories() {
-  #   final List<File> alts = alternateObjectDirectories;
-  #   if (alts == null)
-  #     return null;
-  #   return alts.toArray(new File[0]);
-  # }
-  #
-  # /**
-  #  * Force the repository to be treated as bare (have no working directory).
-  #  * <p>
-  #  * If bare the working directory aspects of the repository won't be
-  #  * configured, and will not be accessible.
-  #  *
-  #  * @return {@code this} (for chaining calls).
-  #  */
-  # public B setBare() {
-  #   setIndexFile(null);
-  #   setWorkTree(null);
-  #   bare = true;
-  #   return self();
-  # }
-  #
-  # /**
-  #  * Whether this repository was forced bare by {@link #setBare()}.
-  #  *
-  #  * @return true if this repository was forced bare by {@link #setBare()}.
-  #  */
-  # public boolean isBare() {
-  #   return bare;
-  # }
-  #
-  # /**
-  #  * Require the repository to exist before it can be opened.
-  #  *
-  #  * @param mustExist
-  #  *            true if it must exist; false if it can be missing and created
-  #  *            after being built.
-  #  * @return {@code this} (for chaining calls).
-  #  */
-  # public B setMustExist(boolean mustExist) {
-  #   this.mustExist = mustExist;
-  #   return self();
-  # }
-  #
-  # /**
-  #  * Whether the repository must exist before being opened.
-  #  *
-  #  * @return true if the repository must exist before being opened.
-  #  */
-  # public boolean isMustExist() {
-  #   return mustExist;
-  # }
-  #
-  # /**
-  #  * Set the top level directory of the working files.
-  #  *
-  #  * @param workTree
-  #  *            {@code GIT_WORK_TREE}, the working directory of the checkout.
-  #  * @return {@code this} (for chaining calls).
-  #  */
-  # public B setWorkTree(File workTree) {
-  #   this.workTree = workTree;
-  #   return self();
-  # }
-  #
-  # /**
-  #  * Get the work tree directory, or null if not set.
-  #  *
-  #  * @return the work tree directory, or null if not set.
-  #  */
-  # public File getWorkTree() {
-  #   return workTree;
-  # }
-  #
-  # /**
-  #  * Set the local index file that is caching checked out file status.
-  #  * <p>
-  #  * The location of the index file tracking the status information for each
-  #  * checked out file in {@code workTree}. This may be null to assume the
-  #  * default {@code gitDiir/index}.
-  #  *
-  #  * @param indexFile
-  #  *            {@code GIT_INDEX_FILE}, the index file location.
-  #  * @return {@code this} (for chaining calls).
-  #  */
-  # public B setIndexFile(File indexFile) {
-  #   this.indexFile = indexFile;
-  #   return self();
-  # }
-  #
-  # /**
-  #  * Get the index file location, or null if not set.
-  #  *
-  #  * @return the index file location, or null if not set.
-  #  */
-  # public File getIndexFile() {
-  #   return indexFile;
-  # }
-  #
-  # /**
-  #  * Read standard Git environment variables and configure from those.
-  #  * <p>
-  #  * This method tries to read the standard Git environment variables, such as
-  #  * {@code GIT_DIR} and {@code GIT_WORK_TREE} to configure this builder
-  #  * instance. If an environment variable is set, it overrides the value
-  #  * already set in this builder.
-  #  *
-  #  * @return {@code this} (for chaining calls).
-  #  */
-  # public B readEnvironment() {
-  #   return readEnvironment(SystemReader.getInstance());
-  # }
-  #
-  # /**
-  #  * Read standard Git environment variables and configure from those.
-  #  * <p>
-  #  * This method tries to read the standard Git environment variables, such as
-  #  * {@code GIT_DIR} and {@code GIT_WORK_TREE} to configure this builder
-  #  * instance. If a property is already set in the builder, the environment
-  #  * variable is not used.
-  #  *
-  #  * @param sr
-  #  *            the SystemReader abstraction to access the environment.
-  #  * @return {@code this} (for chaining calls).
-  #  */
-  # public B readEnvironment(SystemReader sr) {
-  #   if (getGitDir() == null) {
-  #     String val = sr.getenv(GIT_DIR_KEY);
-  #     if (val != null)
-  #       setGitDir(new File(val));
-  #   }
-  #
-  #   if (getObjectDirectory() == null) {
-  #     String val = sr.getenv(GIT_OBJECT_DIRECTORY_KEY);
-  #     if (val != null)
-  #       setObjectDirectory(new File(val));
-  #   }
-  #
-  #   if (getAlternateObjectDirectories() == null) {
-  #     String val = sr.getenv(GIT_ALTERNATE_OBJECT_DIRECTORIES_KEY);
-  #     if (val != null) {
-  #       for (String path : val.split(File.pathSeparator))
-  #         addAlternateObjectDirectory(new File(path));
-  #     }
-  #   }
-  #
-  #   if (getWorkTree() == null) {
-  #     String val = sr.getenv(GIT_WORK_TREE_KEY);
-  #     if (val != null)
-  #       setWorkTree(new File(val));
-  #   }
-  #
-  #   if (getIndexFile() == null) {
-  #     String val = sr.getenv(GIT_INDEX_FILE_KEY);
-  #     if (val != null)
-  #       setIndexFile(new File(val));
-  #   }
-  #
-  #   if (ceilingDirectories == null) {
-  #     String val = sr.getenv(GIT_CEILING_DIRECTORIES_KEY);
-  #     if (val != null) {
-  #       for (String path : val.split(File.pathSeparator))
-  #         addCeilingDirectory(new File(path));
-  #     }
-  #   }
-  #
-  #   return self();
-  # }
-  #
-  # /**
-  #  * Add a ceiling directory to the search limit list.
-  #  * <p>
-  #  * This setting handles one ceiling directory at a time, and is provided to
-  #  * support {@code GIT_CEILING_DIRECTORIES}.
-  #  *
-  #  * @param root
-  #  *            a path to stop searching at; its parent will not be searched.
-  #  * @return {@code this} (for chaining calls).
-  #  */
-  # public B addCeilingDirectory(File root) {
-  #   if (root != null) {
-  #     if (ceilingDirectories == null)
-  #       ceilingDirectories = new LinkedList<>();
-  #     ceilingDirectories.add(root);
-  #   }
-  #   return self();
-  # }
-  #
-  # /**
-  #  * Add ceiling directories to the search list.
-  #  * <p>
-  #  * This setting handles several ceiling directories at once, and is provided
-  #  * to support {@code GIT_CEILING_DIRECTORIES}.
-  #  *
-  #  * @param inList
-  #  *            directory paths to stop searching at. The collection's
-  #  *            contents is copied to an internal list.
-  #  * @return {@code this} (for chaining calls).
-  #  */
-  # public B addCeilingDirectories(Collection<File> inList) {
-  #   if (inList != null) {
-  #     for (File path : inList)
-  #       addCeilingDirectory(path);
-  #   }
-  #   return self();
-  # }
-  #
-  # /**
-  #  * Add ceiling directories to the search list.
-  #  * <p>
-  #  * This setting handles several ceiling directories at once, and is provided
-  #  * to support {@code GIT_CEILING_DIRECTORIES}.
-  #  *
-  #  * @param inList
-  #  *            directory paths to stop searching at. The array's contents is
-  #  *            copied to an internal list.
-  #  * @return {@code this} (for chaining calls).
-  #  */
-  # public B addCeilingDirectories(File[] inList) {
-  #   if (inList != null) {
-  #     for (File path : inList)
-  #       addCeilingDirectory(path);
-  #   }
-  #   return self();
-  # }
-  #
+  defstruct git_dir: nil,
+            object_dir: nil,
+            alternate_object_directories: nil,
+            bare?: false,
+            must_exist?: false,
+            work_tree: nil,
+            index_file: nil,
+            ceiling_directories: nil
+
+  alias Xgit.Lib.Constants
+  alias Xgit.Util.SystemReader
+
+  @doc ~S"""
+  Read standard git environment variables and configure from those.
+
+  This method tries to read the standard git environment variables, such as
+  `GIT_DIR` and `GIT_WORK_TREE` to configure this builder struct. If a property
+  is already set in the struct, the environment variable is not used.
+
+  Returns an updated copy of the builder struct.
+  """
+  def read_environment(%__MODULE__{} = builder, system_reader \\ nil) do
+    builder
+    |> maybe_update_git_dir(system_reader)
+    |> maybe_update_object_dir(system_reader)
+    |> maybe_update_alternate_object_directories(system_reader)
+    |> maybe_update_work_tree(system_reader)
+    |> maybe_update_index_file(system_reader)
+    |> maybe_update_ceiling_directories(system_reader)
+  end
+
+  defp maybe_update_git_dir(%__MODULE__{git_dir: nil} = builder, reader),
+    do: %{builder | git_dir: SystemReader.get_env(reader, Constants.git_dir_key())}
+
+  defp maybe_update_git_dir(builder, _), do: builder
+
+  defp maybe_update_object_dir(%__MODULE__{object_dir: nil} = builder, reader),
+    do: %{
+      builder
+      | object_dir: SystemReader.get_env(reader, Constants.git_object_directory_key())
+    }
+
+  defp maybe_update_object_dir(builder, _), do: builder
+
+  defp maybe_update_alternate_object_directories(
+         %__MODULE__{alternate_object_directories: nil} = builder,
+         reader
+       ) do
+    case SystemReader.get_env(reader, Constants.git_alternate_object_directories_key()) do
+      nil ->
+        builder
+
+      x ->
+        %{builder | alternate_object_directories: String.split(x, ":")}
+        # WINDOWS PORTING NOTE: Should be ";" for Windows.
+    end
+  end
+
+  defp maybe_update_alternate_object_directories(builder, _), do: builder
+
+  defp maybe_update_work_tree(%__MODULE__{work_tree: nil} = builder, reader),
+    do: %{builder | work_tree: SystemReader.get_env(reader, Constants.git_work_tree_key())}
+
+  defp maybe_update_work_tree(builder, _), do: builder
+
+  defp maybe_update_index_file(%__MODULE__{index_file: nil} = builder, reader),
+    do: %{builder | index_file: SystemReader.get_env(reader, Constants.git_index_file_key())}
+
+  defp maybe_update_index_file(builder, _), do: builder
+
+  defp maybe_update_ceiling_directories(%__MODULE__{ceiling_directories: nil} = builder, reader) do
+    case SystemReader.get_env(reader, Constants.git_ceiling_directories_key()) do
+      nil ->
+        builder
+
+      x ->
+        %{builder | ceiling_directories: String.split(x, ":")}
+        # WINDOWS PORTING NOTE: Should be ";" for Windows.
+    end
+  end
+
+  defp maybe_update_ceiling_directories(builder, _), do: builder
+
   # /**
   #  * Configure {@code GIT_DIR} by searching up the file system.
   #  * <p>
@@ -485,6 +164,46 @@ defmodule Xgit.Lib.BaseRepositoryBuilder do
   #   }
   #   return self();
   # }
+  #
+  # private static boolean isSymRef(byte[] ref) {
+  #   if (ref.length < 9)
+  #     return false;
+  #   return /**/ref[0] == 'g' //
+  #       && ref[1] == 'i' //
+  #       && ref[2] == 't' //
+  #       && ref[3] == 'd' //
+  #       && ref[4] == 'i' //
+  #       && ref[5] == 'r' //
+  #       && ref[6] == ':' //
+  #       && ref[7] == ' ';
+  # }
+  #
+  # private static File getSymRef(File workTree, File dotGit, FS fs)
+  #     throws IOException {
+  #   byte[] content = IO.readFully(dotGit);
+  #   if (!isSymRef(content))
+  #     throw new IOException(MessageFormat.format(
+  #         JGitText.get().invalidGitdirRef, dotGit.getAbsolutePath()));
+  #
+  #   int pathStart = 8;
+  #   int lineEnd = RawParseUtils.nextLF(content, pathStart);
+  #   while (content[lineEnd - 1] == '\n' ||
+  #          (content[lineEnd - 1] == '\r' && SystemReader.getInstance().isWindows()))
+  #     lineEnd--;
+  #   if (lineEnd == pathStart)
+  #     throw new IOException(MessageFormat.format(
+  #         JGitText.get().invalidGitdirRef, dotGit.getAbsolutePath()));
+  #
+  #   String gitdirPath = RawParseUtils.decode(content, pathStart, lineEnd);
+  #   File gitdirFile = fs.resolve(workTree, gitdirPath);
+  #   if (gitdirFile.isAbsolute())
+  #     return gitdirFile;
+  #   else
+  #     return new File(workTree, gitdirPath).getCanonicalFile();
+  # }
+  #
+  # /** Configuration file of target repository, lazily loaded if required. */
+  # private Config config;
   #
   # /**
   #  * Guess and populate all parameters not already defined.
@@ -680,24 +399,6 @@ defmodule Xgit.Lib.BaseRepositoryBuilder do
   #   return null;
   # }
   #
-  # /**
-  #  * Get the configured FS, or {@link FS#DETECTED}.
-  #  *
-  #  * @return the configured FS, or {@link FS#DETECTED}.
-  #  */
-  # protected FS safeFS() {
-  #   return getFS() != null ? getFS() : FS.DETECTED;
-  # }
-  #
-  # /**
-  #  * Get this object
-  #  *
-  #  * @return {@code this}
-  #  */
-  # @SuppressWarnings("unchecked")
-  # protected final B self() {
-  #   return (B) this;
-  # }
 
   @doc ~S"""
   blah blah blah
