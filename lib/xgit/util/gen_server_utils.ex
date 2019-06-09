cb72f71cec1639463fe33bb3355b2ee689b62437
Eric Scouten <eric@scouten.com>
2019-06-09 15:39:22 -0700

    Add GenServerUtils.wrap_call/4.

--- lib/xgit/util/gen_server_utils.ex
+++ lib/xgit/util/gen_server_utils.ex
@@ -63,9 +63,31 @@ defmodule Xgit.Util.GenServerUtils do
   end
 
   @doc ~S"""
+  Wrap a `handle_call/3` call to a `handle_(something)` call on a module.
+
+  Wraps common `:ok` and error responses and exceptions and returns them to caller.
+
+  Should be used for standalone modules (i.e. modules that are not open to extension).
+  """
+  def wrap_call(mod, function, args, prev_state) do
+    try do
+      case apply(mod, function, args) do
+        {:ok, state} -> {:reply, :ok, state}
+        {:ok, response, state} -> {:reply, {:ok, response}, state}
+        {:error, reason, state} -> {:reply, {:error, reason}, state}
+      end
+    rescue
+      e -> {:reply, {:error, e}, prev_state}
+    end
+  end
+
+  @doc ~S"""
   Delegate a `handle_call/3` call to a `handle_(something)` call on a module.
 
   Wraps common `:ok` and error responses and exceptions and returns them to caller.
+
+  Unlike `wrap_call/4`, assumes that the `GenServer` state is a tuple of
+  `{module, mod_state}` and re-wraps module state accordingly.
   """
   def delegate_call_to(mod, function, args, mod_state) do
     try do
