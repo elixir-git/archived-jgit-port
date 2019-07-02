dc0d833edeccee5b5951fe3ea6c67a0f3cdbda78
Eric Scouten <eric@scouten.com>
2019-07-02 08:00:29 -0700

    Doc review Xgit.Lib.FileMode.

--- lib/xgit/lib/file_mode.ex
+++ lib/xgit/lib/file_mode.ex
@@ -47,11 +47,23 @@
 
 defmodule Xgit.Lib.FileMode do
   @moduledoc ~S"""
-  Constants describing various file modes recognized by git.
+  Describes the various file modes recognized by git.
 
-  git uses a subset of the available Unix file permission bits. The `FileMode`
-  module provides access to constants defining the modes actually used by git.
+  git uses a subset of the available Unix file permission bits. This module
+  provides access to constants defining the modes actually used by git.
   """
+
+  @typedoc ~S"""
+  Retains information about a given git file mode.
+
+  ## Struct Members
+
+  * `:mode_bits`: (integer) the actual file mode
+  * `:object_type`: (integer) the file type (as an `obj_*` value from `Xgit.Lib.Constants`)
+  * `:octal_bytes`: (charlist) `:mode_bits` rendered as an octal value
+  """
+  @type t :: %__MODULE__{}
+
   @enforce_keys [:mode_bits, :object_type, :octal_bytes]
   defstruct [:mode_bits, :object_type, :octal_bytes]
 
@@ -60,44 +72,57 @@ defmodule Xgit.Lib.FileMode do
   alias Xgit.Lib.Constants
 
   @doc "Mask to apply to a file mode to obtain its type bits."
+  @spec type_mask :: integer
   def type_mask, do: 0o170000
 
   @doc "Bit pattern for `type_mask/0` matching `tree/0`."
+  @spec type_tree :: integer
   def type_tree, do: 0o040000
 
   @doc "Bit pattern for `type_mask/0` matching `symlink/0`."
+  @spec type_symlink :: integer
   def type_symlink, do: 0o120000
 
   @doc "Bit pattern for `type_mask/0` matching `regular_file/0`."
+  @spec type_file :: integer
   def type_file, do: 0o100000
 
   @doc "Bit pattern for `type_mask/0` matching `gitlink/0`."
+  @spec type_gitlink :: integer
   def type_gitlink, do: 0o160000
 
   @doc "Bit pattern for `type_mask/0` matching `missing/0`."
+  @spec type_missing :: integer
   def type_missing, do: 0o000000
 
   @doc "Mode indicating an entry is a tree (aka directory)."
+  @spec tree :: t
   def tree, do: new(type_tree(), Constants.obj_tree())
 
   @doc "Mode indicating an entry is a symbolic link."
+  @spec symlink :: t
   def symlink, do: new(type_symlink(), Constants.obj_blob())
 
   @doc "Mode indicating an entry is a non-executable file."
+  @spec regular_file :: t
   def regular_file, do: new(0o100644, Constants.obj_blob())
 
   @doc "Mode indicating an entry is an executable file."
+  @spec executable_file :: t
   def executable_file, do: new(0o100755, Constants.obj_blob())
 
   @doc "Mode indicating an entry is a submodule commit in another repository."
+  @spec gitlink :: t
   def gitlink, do: new(type_gitlink(), Constants.obj_commit())
 
   @doc "Mode indicating an entry is missing during parallel walks."
+  @spec missing :: t
   def missing, do: new(type_missing(), Constants.obj_bad())
 
   @doc ~S"""
-  Test a file mode for equality with this `FileMode` struct.
+  Return `true` if the `Xgit.Lib.FileMode` struct matches the file mode value.
   """
+  @spec match_mode_bits?(file_mode :: t, mode_bits :: integer) :: boolean
   def match_mode_bits?(file_mode, mode_bits)
 
   def match_mode_bits?(%__MODULE__{mode_bits: 0o100644}, mode_bits),
@@ -113,8 +138,9 @@ defmodule Xgit.Lib.FileMode do
     do: self_mode_bits == other_mode_bits
 
   @doc ~S"""
-  Convert a set of mode bits into a `FileMode` enumerated value.
+  Convert a set of mode bits into an `Xgit.Lib.FileMode` enumerated value.
   """
+  @spec from_bits(mode_bits :: integer) :: t
   def from_bits(bits), do: bits |> band(type_mask()) |> from_type_bits(bits)
 
   defp from_type_bits(0o000000, 0), do: missing()
